d3c904ec9dba9aa6a6a3016402927fa4bd925886
Cristian Pereyra <cp.crist.pereyra@gmail.com>
2021-08-05 02:50:11 -0400

    Create Transaction Card

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import { IncomeExpenses } from './comps/IncomeExpenses';
 import {TransactionList } from './comps/TransactionList';
 import { AddTransaction } from './comps/AddTransaction';
 
+import { GlobalProvider } from './context/GlobalState';
+
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
       <div className="container">
         <Balance />
@@ -15,7 +17,7 @@ function App() {
         <TransactionList />
         <AddTransaction />
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
