df1685d3578ed5fbf161a1d7b2772bdf84e856e5
Cristian Pereyra <cp.crist.pereyra@gmail.com>
2021-08-05 13:15:17 -0400

    Delete Transactions

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -13,8 +13,13 @@ export const GlobalContext = createContext(initialState);
 
 export const GlobalProvider = ({children}) => {
     const [state, dispatch] = useReducer(AppReducer, initialState);
+    //capture the weather state
+    function deleteTransaction(id) {
+        dispatch({ type: 'DELETE_TRANSACTION', payload: id });
+    }
     return (<GlobalContext.Provider value={{
-        transactions: state.transactions
+        transactions: state.transactions,
+        deleteTransaction
     }}>
         {children}
     </GlobalContext.Provider>)
